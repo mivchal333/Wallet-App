683c473b0ae591e1ab333949e5317eac7eb3e8e6
mivchal <mivchal@gmail.com>
2020-05-09 10:31:58 +0200

    wallet app, migration

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SzkieletoweProjekt.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Wallet-App.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
