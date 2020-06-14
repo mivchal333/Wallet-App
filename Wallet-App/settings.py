aea19aa588ca9f7dd52deed17f62489e980fc6f6
mivchal <mivchal@gmail.com>
2020-06-14 22:18:50 +0200

    env

--- Wallet-App/settings.py
+++ Wallet-App/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = '(e37q6a3l4&0#f9d35lt8ea8lz@j5pbw-!sepj0+q4*+pw%&%h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
